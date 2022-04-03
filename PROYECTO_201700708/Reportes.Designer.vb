ec1daada9ad2624a46d3c41369ce607a166a8be6
WSantos35 <wsantos35.ws@gmail.com>
2022-04-02 23:52:47 -0600

    Agregando Botones reportes

--- PROYECTO_201700708/Reportes.Designer.vb
+++ PROYECTO_201700708/Reportes.Designer.vb
@@ -23,6 +23,12 @@ Partial Class Reportes
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.btnReportes_Regresar = New System.Windows.Forms.Button()
+        Me.btnReporte1 = New System.Windows.Forms.Button()
+        Me.btnReporte2 = New System.Windows.Forms.Button()
+        Me.btnReporte3 = New System.Windows.Forms.Button()
+        Me.btnReporte4 = New System.Windows.Forms.Button()
+        Me.btnReport5 = New System.Windows.Forms.Button()
+        Me.btnReport6 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'btnReportes_Regresar
@@ -34,12 +40,72 @@ Partial Class Reportes
         Me.btnReportes_Regresar.Text = "Regresar"
         Me.btnReportes_Regresar.UseVisualStyleBackColor = True
         '
+        'btnReporte1
+        '
+        Me.btnReporte1.Location = New System.Drawing.Point(38, 48)
+        Me.btnReporte1.Name = "btnReporte1"
+        Me.btnReporte1.Size = New System.Drawing.Size(213, 83)
+        Me.btnReporte1.TabIndex = 1
+        Me.btnReporte1.Text = "CANTIDAD VENTAS POR VENDEDOR"
+        Me.btnReporte1.UseVisualStyleBackColor = True
+        '
+        'btnReporte2
+        '
+        Me.btnReporte2.Location = New System.Drawing.Point(289, 48)
+        Me.btnReporte2.Name = "btnReporte2"
+        Me.btnReporte2.Size = New System.Drawing.Size(213, 83)
+        Me.btnReporte2.TabIndex = 2
+        Me.btnReporte2.Text = "CANTIDAD DE VENTAS POR PRODUCTO"
+        Me.btnReporte2.UseVisualStyleBackColor = True
+        '
+        'btnReporte3
+        '
+        Me.btnReporte3.Location = New System.Drawing.Point(541, 48)
+        Me.btnReporte3.Name = "btnReporte3"
+        Me.btnReporte3.Size = New System.Drawing.Size(213, 83)
+        Me.btnReporte3.TabIndex = 3
+        Me.btnReporte3.Text = "CANTIDAD DE VECES QUE VENDEDOR VENDE UN PRODUCTO"
+        Me.btnReporte3.UseVisualStyleBackColor = True
+        '
+        'btnReporte4
+        '
+        Me.btnReporte4.Location = New System.Drawing.Point(38, 191)
+        Me.btnReporte4.Name = "btnReporte4"
+        Me.btnReporte4.Size = New System.Drawing.Size(213, 83)
+        Me.btnReporte4.TabIndex = 4
+        Me.btnReporte4.Text = "TOP 3 PRODUCTOS MAS VENDIDOS"
+        Me.btnReporte4.UseVisualStyleBackColor = True
+        '
+        'btnReport5
+        '
+        Me.btnReport5.Location = New System.Drawing.Point(289, 191)
+        Me.btnReport5.Name = "btnReport5"
+        Me.btnReport5.Size = New System.Drawing.Size(213, 83)
+        Me.btnReport5.TabIndex = 5
+        Me.btnReport5.Text = "TOPO 3 VENDEDORES CON MAS VENTAS"
+        Me.btnReport5.UseVisualStyleBackColor = True
+        '
+        'btnReport6
+        '
+        Me.btnReport6.Location = New System.Drawing.Point(541, 191)
+        Me.btnReport6.Name = "btnReport6"
+        Me.btnReport6.Size = New System.Drawing.Size(213, 83)
+        Me.btnReport6.TabIndex = 6
+        Me.btnReport6.Text = "TOP 3 VENDEDORES CON MENOS VENTAS"
+        Me.btnReport6.UseVisualStyleBackColor = True
+        '
         'Reportes
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
         Me.ControlBox = False
+        Me.Controls.Add(Me.btnReport6)
+        Me.Controls.Add(Me.btnReport5)
+        Me.Controls.Add(Me.btnReporte4)
+        Me.Controls.Add(Me.btnReporte3)
+        Me.Controls.Add(Me.btnReporte2)
+        Me.Controls.Add(Me.btnReporte1)
         Me.Controls.Add(Me.btnReportes_Regresar)
         Me.Name = "Reportes"
         Me.Text = "Reportes"
@@ -48,4 +114,10 @@ Partial Class Reportes
     End Sub
 
     Friend WithEvents btnReportes_Regresar As Button
+    Friend WithEvents btnReporte1 As Button
+    Friend WithEvents btnReporte2 As Button
+    Friend WithEvents btnReporte3 As Button
+    Friend WithEvents btnReporte4 As Button
+    Friend WithEvents btnReport5 As Button
+    Friend WithEvents btnReport6 As Button
 End Class
